37b393129055c80b54473f5ec3bc8dd22d6c7a42
Eero Mutka <eero.mutka@gmail.com>
2024-08-14 13:36:37 +0200

    Networked movement and hands

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
 	
 	public GameObject handViz;
@@ -12,43 +13,69 @@ public class Player : MonoBehaviour
 	float prevMouseX;
 	float prevMouseY;
 	
-	Vector2 armCircle;
+	
+	int localFrameIndex = 0;
+	
+	// networked variables, automatically replicated from server to clients
+	// -transform.position is implicitly like this
+	public NetworkVariable<Vector2> armPos2D;
+	
+	// server-only variables
+	float lastInputMoveX = 0f;
+	float lastInputMoveY = 0f;
+	
+	// owning-client-only variables
+	Vector2 clientArmPos2D;
 	
 	void Start()
 	{
 		characterController = GetComponent<CharacterController>();
 	}
 	
+	[Rpc(SendTo.Server, Delivery = RpcDelivery.Unreliable)]
+	void SendInputToServerRpc(float moveX, float moveY, float armPosX, float armPosY) {
+		lastInputMoveX = moveX;
+		lastInputMoveY = moveY;
+		armPos2D.Value = new Vector2(armPosX, armPosY);
+	}
+	
 	void Update()
 	{
-		float mouseX = Input.mousePosition.x / (float)Screen.height;
-		float mouseY = Input.mousePosition.y / (float)Screen.height;
-		float mouseDx = mouseX - prevMouseX;
-		float mouseDy = mouseY - prevMouseY;
-		
-		armCircle += 3f * new Vector2(mouseDx, mouseDy);
-		if (armCircle.magnitude > 1) {
-			armCircle = armCircle.normalized;
+		if (IsServer) {
+			Vector3 move = new Vector3(lastInputMoveX, 0, lastInputMoveY);
+			Debug.Log($"Server move: {move}");
+			characterController.Move(move * Time.deltaTime * 5f);
 		}
 		
-		float theta = armCircle.magnitude*2f;
-		float z = Mathf.Cos(theta);
-		float x = Mathf.Sin(theta);
+		if (IsClient && IsOwner && Application.isFocused) {
+			float mouseX = Input.mousePosition.x / (float)Screen.height;
+			float mouseY = Input.mousePosition.y / (float)Screen.height;
+			float mouseDx = mouseX - prevMouseX;
+			float mouseDy = mouseY - prevMouseY;
+			
+			clientArmPos2D += 3f * new Vector2(mouseDx, mouseDy);
+			if (clientArmPos2D.magnitude > 1) {
+				clientArmPos2D = clientArmPos2D.normalized;
+			}
+			
+			float moveX = Input.GetAxis("Horizontal");
+			float moveY = Input.GetAxis("Vertical");
+			SendInputToServerRpc(moveX, moveY, clientArmPos2D.x, clientArmPos2D.y);
+			
+			prevMouseX = mouseX;
+			prevMouseY = mouseY;
+		}
 		
-		Vector2 armCircleNorm = armCircle.normalized;
-		Vector3 armSphere = new Vector3(armCircleNorm.x, 0, armCircleNorm.y)*x + new Vector3(0, z, 0);
-		
-		float yaw = Mathf.Atan2(armCircle.y, armCircle.x);
-		
-		// 3d position
-		
-		// arm local position in 3D... we need to project from
-		handViz.transform.localPosition = armSphere;//new Vector3(armCircle.x, 0, armCircle.y);
-		
-		prevMouseX = mouseX;
-		prevMouseY = mouseY;
-		
-		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		characterController.Move(move * Time.deltaTime * 5f);
+		// draw hand
+		if (IsClient) {
+			float theta = armPos2D.Value.magnitude*2f;
+			float z = Mathf.Cos(theta);
+			float x = Mathf.Sin(theta);
+			
+			Vector2 armPos2DNorm = armPos2D.Value.normalized;
+			Vector3 armPos3D = new Vector3(armPos2DNorm.x, 0, armPos2DNorm.y)*x + new Vector3(0, z, 0);
+			
+			handViz.transform.localPosition = armPos3D;
+		}
 	}
 }
---
+++ Assets/Scripts/WhackballManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class WhackballManager : MonoBehaviour
+{
+	NetworkManager networkManager;
+	
+	void Awake()
+	{
+		networkManager = GetComponent<NetworkManager>();
+	}
+
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+		if (!networkManager.IsClient && !networkManager.IsServer)
+		{
+			StartButtons();
+		}
+		else
+		{
+			StatusLabels();
+			SubmitNewPosition();
+		}
+
+		GUILayout.EndArea();
+	}
+
+	void StartButtons()
+	{
+		if (GUILayout.Button("Host")) networkManager.StartHost();
+		if (GUILayout.Button("Client")) networkManager.StartClient();
+		if (GUILayout.Button("Server")) networkManager.StartServer();
+	}
+	
+	void StatusLabels()
+	{
+		var mode = networkManager.IsHost ?
+			"Host" : networkManager.IsServer ? "Server" : "Client";
+
+		GUILayout.Label("Transport: " +
+			networkManager.NetworkConfig.NetworkTransport.GetType().Name);
+		GUILayout.Label("Mode: " + mode);
+	}
+	
+	void SubmitNewPosition()
+	{
+		/*if (GUILayout.Button(networkManager.IsServer ? "Move" : "Request Position Change"))
+		{
+			if (networkManager.IsServer && !networkManager.IsClient)
+			{
+				foreach (ulong uid in networkManager.ConnectedClientsIds)
+					networkManager.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().Move();
+			}
+			else
+			{
+				var playerObject = networkManager.SpawnManager.GetLocalPlayerObject();
+				var player = playerObject.GetComponent<Player>();
+				player.Move();
+			}
+		}*/
+	}
+}
